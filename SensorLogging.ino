6a1d61be50606d39287e528123a8ecfb4f6c5ab1
denailz <62827956+denailz@users.noreply.github.com>
2020-12-08 13:21:11 -0800

    Add files via upload
    Included rotation and accelleration

--- SensorLogging.ino
+++ SensorLogging.ino
@@ -7,48 +7,76 @@
 #include <Adafruit_BMP280.h>
 #include <SFE_BMP180.h>
 #include <Adafruit_BMP280.h>
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
 #include <SPI.h>
 #include <SD.h>
 File myFile;
 
 Adafruit_BMP280 bmp; // I2C Interface
-float initPressure = 0.00;
+Adafruit_MPU6050 mpu;
 
+//Initialize some global variables for altitude calculation
+float initPressure = 0.00;
 
 void setup() {
   Serial.begin(9600);
+  
   Serial.println(F("BMP280 test"));
 
+  //BMP
   if (!bmp.begin()) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
-
-  /* Default settings from datasheet. */
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
+
+//MPU
+ Serial.println("Adafruit MPU6050 test");
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+  
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
+
+
+//SD CARD
 Serial.println("Initializing SD card...");
   if (!SD.begin(10)) {
     Serial.println("initialization failed!");
 
   }
-  Serial.println("initialization done.");
 
+//Redefine variables for altitude calculation, sets starting pressure as 0 ft altitude
 initPressure = (bmp.readPressure()/100.00);
 Serial.print(F("Initial Pressure = "));
 Serial.print((initPressure*100)*0.0002953);
 Serial.println(" inHg");
+
 }
 
 
 void loop() {
 
     myFile = SD.open("test.csv", FILE_WRITE);
-   
+
+//BMP
   /*
     Serial.print(F("Temperature = "));
     Serial.print(bmp.readTemperature());
@@ -64,7 +92,33 @@ void loop() {
 
     myFile.print(3.28084*((bmp.readAltitude(initPressure))));
     myFile.print(",");
-    
+
+//MPU
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /*
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  */
+
+  //Acceleration Vector Through the Board
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print(" Roll Rate: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Pitch Rate: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Yaw Rate: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+    Serial.println("");
+
     delay(500);
 
     myFile.close();
