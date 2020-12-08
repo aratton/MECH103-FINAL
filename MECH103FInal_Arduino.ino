6f33af6eef99c9b858a43e5fb2b3ac39ee288893
denailz <62827956+denailz@users.noreply.github.com>
2020-12-08 12:12:57 -0800

    Add files via upload
    commented out unecessary data, added print to serial for altitude

--- MECH103FInal_Arduino.ino
+++ MECH103FInal_Arduino.ino
@@ -1,3 +1,5 @@
+#include <BMP180MI.h>
+
 //Alex Ratton
 //Last Modified December 6 2020
 //MECH103 Final Project
@@ -10,7 +12,7 @@
 #include <SD.h>
 File myFile;
 SFE_BMP180 pressure;
-#define ALTITUDE 260.0
+#define ALTITUDE 103.63 //Pleasanton, CA
 void setup()
 {
   Serial.begin(9600);
@@ -46,14 +48,14 @@ void setup()
 
 }
 void loop() {
-  myFile = SD.open("test.txt", FILE_WRITE);
+  myFile = SD.open("test.csv", FILE_WRITE);
   char status;
   double T, P, p0, a;
-  myFile.print("provided altitude: ");
-  myFile.print(ALTITUDE, 0);
-  myFile.print(" meters, ");
-  myFile.print(ALTITUDE * 3.28084, 0);
-  myFile.print(" feet");
+  //myFile.print("provided altitude: ");
+  //myFile.print(ALTITUDE, 0);
+  //myFile.print(" meters, ");
+  //myFile.print(ALTITUDE * 3.28084, 0);
+  //myFile.print(" feet");
   status = pressure.startTemperature();
   if (status != 0)
   {
@@ -64,7 +66,7 @@ void loop() {
     // Note that the measurement is stored in the variable T.
     // Function returns 1 if successful, 0 if failure.
 
-    status = pressure.getTemperature(T);
+    /*status = pressure.getTemperature(T);
     if (status != 0)
     {
       // Print out the measurement:
@@ -73,7 +75,7 @@ void loop() {
       myFile.print(" deg C, ");
       myFile.print((9.0 / 5.0)*T + 32.0, 2);
       myFile.print(" deg F");
-
+*/
       // Start a pressure measurement:
       // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
       // If request is successful, the number of ms to wait is returned.
@@ -95,25 +97,25 @@ void loop() {
         if (status != 0)
         {
           // Print out the measurement:
-          myFile.print("absolute pressure: ");
+          /*myFile.print("absolute pressure: ");
           myFile.print(P, 2);
           myFile.print(" mb, ");
           myFile.print(P * 0.0295333727, 2);
           myFile.print(" inHg");
-
+*/
           // The pressure sensor returns abolute pressure, which varies with altitude.
           // To remove the effects of altitude, use the sealevel function and your current altitude.
           // This number is commonly used in weather reports.
           // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
           // Result: p0 = sea-level compensated pressure in mb
 
-          p0 = pressure.sealevel(P, ALTITUDE); // we're at 90 meters (Boulder, CO)
-          myFile.print("relative (sea-level) pressure: ");
+          p0 = pressure.sealevel(P, ALTITUDE); // 
+/*          myFile.print("relative (sea-level) pressure: ");
           myFile.print(p0, 2);
           myFile.print(" mb, ");
           myFile.print(p0 * 0.0295333727, 2);
           myFile.print(" inHg");
-
+*/
           // On the other hand, if you want to determine your altitude from the pressure reading,
           // use the altitude function along with a baseline pressure (sea-level or other).
           // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
@@ -125,16 +127,22 @@ void loop() {
           myFile.print(" meters, ");
           myFile.print(a * 3.28084, 0);
           myFile.print(" feet");
+
+          Serial.println("computed altitude: ");
+          Serial.print(a, 0);
+          Serial.println(" meters, ");
+          Serial.print(a * 3.28084, 0);
+          Serial.println(" feet");
         }
         else myFile.print("error retrieving pressure measurement");
       }
       else myFile.print("error starting pressure measurement");
     }
     else myFile.print("error retrieving temperature measurement");
-  }
-  else myFile.print("error starting temperature measurement");
+  
+  //}else myFile.print("error starting temperature measurement");
 
-  delay(5000);  // Pause for 5 seconds.
+  delay(500);  // Pause for 5 seconds.
 
   myFile.close();
 
